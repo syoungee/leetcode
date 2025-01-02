a18af9e120a8ba722ac2db792ace41d4c9b2534c
syoungee <sunyoungah69@gmail.com>
2025-01-02 13:45:56 +0900

    7번

---
+++ 7.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+  // 32비트 정수의 범위
+  const MIN = -Math.pow(2, 31); // -2147483648
+  const MAX = Math.pow(2, 31) - 1; // 2147483647
+
+  let isMinus = false;
+  if (x < 0) {
+    isMinus = true;
+    x *= -1;
+  }
+
+  let y = x.toString();
+  let z = y.split('').reverse().join('');
+  let num = parseInt(z);
+
+  // 부호 복구
+  if (isMinus) num = -num;
+
+  // 범위를 벗어난 경우 0 반환
+  if (num < MIN || num > MAX) {
+    return 0;
+  }
+
+  return num;
+};
+
+console.log(reverse(-123));
